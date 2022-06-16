c93895054d738a854a0e5b3662c38369a7a194fa
Rodolfo Arruda Prado <addodorfo@gmail.com>
2022-06-16 19:25:11 -0300

    primeiro commit, adicionando programa que converte a string

---
+++ Programa.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Programa {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.nextLine();
+
+		String output = "";
+		int length = s.length() - 1;
+		for(int i = length; i >= 0; i--) {
+			char charAt = s.charAt(i);
+			String valueOf = String.valueOf(charAt);
+			output = output.concat(valueOf);
+		}
+		System.out.print(output);
+	}
+}
